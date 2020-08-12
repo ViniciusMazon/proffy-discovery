8eae2ac1543548cee7820ce8a19275ec680ad49d
Vinicius Mazon <vnpmazon@gmail.com>
2020-08-12 13:04:37 -0300

    feat(front-end): Add the script to add new fields on the Give Class page

---
+++ front-end/scripts/addField.js
@@ -0,0 +1,12 @@
+document.querySelector("#add-time")
+  .addEventListener('click', cloneField);
+
+function cloneField() {
+  const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+
+  const fields = newFieldContainer.querySelectorAll('input');
+  fields.forEach((field) => {
+    field.value = "";
+  })
+  document.querySelector("#schedule-items").appendChild(newFieldContainer);
+}
